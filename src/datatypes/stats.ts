ffc0567d181e91e22b3da18690bd18df41e6fe80
FebeFarina <alu0101430973@ull.edu.es>
2023-03-16 18:03:11 +0000

    Odio

--- src/datatypes/stats.ts
+++ src/datatypes/stats.ts
@@ -13,4 +13,42 @@ export class Stats {
     this.year_distance = 0;
     this.year_slope = 0;
   }
+  getWeekDistance() {
+    return this.week_distance;
+  }
+  getWeekSlope() {
+    return this.week_slope;
+  }
+  getMonthDistance() {
+    return this.month_distance;
+  }
+  getMonthSlope() {
+    return this.month_slope;
+  }
+  getYearDistance() {
+    return this.year_distance;
+  }
+  getYearSlope() {
+    return this.year_slope;
+  }
+  updateStats(distance: number, slope: number) {
+    this.week_distance += distance;
+    this.week_slope += slope;
+    this.month_distance += distance;
+    this.month_slope += slope;
+    this.year_distance += distance;
+    this.year_slope += slope;
+  }
+  newWeek() {
+    this.week_distance = 0;
+    this.week_slope = 0;
+  }
+  newMonth() {
+    this.month_distance = 0;
+    this.month_slope = 0;
+  }
+  newYear() {
+    this.year_distance = 0;
+    this.year_slope = 0;
+  }
 }
--- tests/jsonadapters/jsonusuarios.ts
+++
@@ -1,9 +0,0 @@
-import "mocha";
-import { expect } from "chai";
-import { JsonUsuario } from "../../src/jsonadapters/jsonusuarios";
-
-describe("JsonUsuario", () => {
-  it("Debe existir la clase JsonUsuario", () => {
-    expect(JsonUsuario).to.exist;
-  });
-});

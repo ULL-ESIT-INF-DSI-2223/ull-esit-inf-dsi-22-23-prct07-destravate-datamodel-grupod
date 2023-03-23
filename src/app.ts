edea737a913853b55ba7b0d247a77d85c4acda73
FebeFarina <alu0101430973@ull.edu.es>
2023-03-23 11:09:09 +0000

    SI

--- src/app.ts
+++ src/app.ts
@@ -221,7 +221,6 @@ export class App {
           case Commandos.EliminarReto:
             this.eliminarReto();
             break;
-
           case Commandos.Salir:
             console.log("Hasta luego!");
             break;

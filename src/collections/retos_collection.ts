f1c66ecd6007fe5e8b9029ba63d3fd3270471d53
Daniel González de Chaves González <alu0101407951@ull.edu.es>
2023-03-23 12:52:19 +0000

    Cosas

--- src/collections/retos_collection.ts
+++ src/collections/retos_collection.ts
@@ -5,12 +5,4 @@ export class RetoCollection extends BaseCollection<Reto> {
   constructor() {
     super();
   }
-
-  getReto(idRuta: number): Reto {
-    if (this.getElement(idRuta) === undefined) {
-      throw new Error("No existe el reto");
-    } else {
-      return this.getElement(idRuta);
-    }
-  }
 }
